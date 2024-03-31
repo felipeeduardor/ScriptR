32d3ea08132250b5df7fbeae530b163c74ba7f66
Felipe Eduardo <140353629+felipeeduardor@users.noreply.github.com>
2024-03-31 13:56:56 -0300

    novo

---
+++ Meus Projetos.R
@@ -0,0 +1,8 @@
+# Criar novas variáveis
+
+setwd("C:\\Users\\nelzi\\OneDrive\\Área de Trabalho\\R")
+
+# Base de dados com carros da tabela fipe em junho de 2018
+df <-read.csv("C:\\Users\\nelzi\\OneDrive\\Área de Trabalho\\R\\FipeTransforman.csv",header=TRUE,encoding="UTF-8")
+
+View(df)
