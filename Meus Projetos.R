078384b01640a89b7e30eeca1789bb38127d93dd
Felipe Eduardo <140353629+felipeeduardor@users.noreply.github.com>
2024-03-31 15:39:45 -0300

    Update Meus Projetos.R

--- Meus Projetos.R
+++ Meus Projetos.R
@@ -6,3 +6,44 @@ setwd("C:\\Users\\nelzi\\OneDrive\\Área de Trabalho\\R")
 df <-read.csv("C:\\Users\\nelzi\\OneDrive\\Área de Trabalho\\R\\FipeTransforman.csv",header=TRUE,encoding="UTF-8")
 
 View(df)
+
+levels(df$Carro)
+
+# coluna com cilindradas
+library(stringr)
+?str_extract
+
+df$Cilindradas <- str_extract(df$Carro, "[0-9]\\.[0-9]")
+str(df$Cilindradas)
+
+df$Cilindradas <- as.factor(df$Cilindradas)
+str(df$Cilindradas)
+summary(df$Cilindradas)
+
+
+# verificar linhas NA
+unique(df$Carro[is.na(df$Cilindradas)])
+
+library(dplyr)
+
+df %>%
+  filter(is.na(Cilindradas)) %>%
+  distinct(Carro)
+
+# coluna tipo de câmbio
+?str_detect
+?subset
+
+
+aut <-subset(df,str_detect(df$Carro, "Aut\\."),"Carro")
+aut <-unique(aut)
+aut$Cambio <- "aut"
+
+View(aut)
+
+?left_join
+
+
+df <- left_join(df,aut)
+df$Cambio[is.na(df$Cambio)] <- "Mec"
+str(df$Cambio)
