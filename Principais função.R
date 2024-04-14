6d97aca6aa9f520639e3c2e9fce802db56d1ad26
Felipe Eduardo <140353629+felipeeduardor@users.noreply.github.com>
2024-04-14 09:03:54 -0300

    Estudos

---
+++ Principais função.R
@@ -0,0 +1,24 @@
+# Head e tail
+esoph
+#default 6 linhas
+head(esoph)
+tail(esoph)
+
+#10 linhas
+head(esoph, n=10)
+
+#Dimensões
+dim(esoph)
+
+#se usar com objeto bidimensional vai retornar colunas
+length(islands)
+
+# Nomes de colunas 
+colnames(esoph)
+
+# Nomes de Linhas
+rownames(esoph)
+
+# Resumo 
+summary(esoph)
+
