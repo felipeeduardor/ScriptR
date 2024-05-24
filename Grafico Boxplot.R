6e9522182f085d543929eaad1a19b2706d95d688
Felipe Eduardo <140353629+felipeeduardor@users.noreply.github.com>
2024-05-23 21:30:50 -0300

    graficos

--- Grafico Boxplot.R
+++ Grafico Boxplot.R
@@ -17,3 +17,19 @@ boxplot(trees)
 
 #Agregação
 InsectSprays
+spray = aggregate(.~spray,data = InsectSprays,sum)
+spray
+
+#Grafico de barras
+# Executar com ctrl+ shift+enter
+barplot(spray$count,col=gray.colors(6), xlab = "spray",ylab = "Total",names.arg = spray$spray)
+box()
+
+#Gráfico de Setor - pizza
+
+pie(spray$count, labels = spray$spray, main = "Spray",col = c(1:6))
+
+
+# pizza com legenda
+pie(spray$count,labels = NA, main = "Spray",col =c(1:6))
+legend('bottomright',legend = spray$spray,cex = 1,fill = c(1:6))
